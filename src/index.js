6fac28de37e92946c40c6bd8909578356f99267e
Nguyen Quang Viet <vietnq@zigexn.vn>
2023-08-05 11:55:50 +0700

    make Default layout

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from '~/App';
 import reportWebVitals from './reportWebVitals';
 
+import GlobalStyles from '~/Components/GlobalStyles';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles>
+      <App />
+    </GlobalStyles>
   </React.StrictMode>,
 );
 
