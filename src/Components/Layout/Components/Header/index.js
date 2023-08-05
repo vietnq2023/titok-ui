2e761c7e52df76cc60cd0e896e6dc7af5d4716cc
Nguyen Quang Viet <vietnq@zigexn.vn>
2023-08-05 22:15:31 +0700

    Make UI for header

--- src/Components/Layout/Components/Header/index.js
+++ src/Components/Layout/Components/Header/index.js
@@ -1,14 +1,29 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
-
 function Header() {
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
-        {/* logo
-      search box */}
+        <div className={cx('logo')}>
+          <img src={images.logo} alt="Tiktok" />
+        </div>
+        <div className={cx('search')}>
+          <input placeholder="Search" spellCheck={false} />
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+          <button className={cx('search-btn')}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className={cx('action')}>viet dep trai</div>
       </div>
     </header>
   );
