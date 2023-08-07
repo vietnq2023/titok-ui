8da5cb83fb856cb6149ece4bec1da8b4bc503de8
Nguyen Quang Viet <vietnq@zigexn.vn>
2023-08-07 10:49:36 +0700

    logic header #3

--- src/Components/Layout/Components/Search/index.js
+++ src/Components/Layout/Components/Search/index.js
@@ -7,6 +7,7 @@ import { Wrapper as PopperWrapper } from '~/Components/Popper';
 import { SearchIcon } from '~/Components/Icons';
 import styles from './Search.module.scss';
 import AccountItem from '../AccountItem';
+import { useDebounce } from '~/hooks';
 
 const cx = classNames.bind(styles);
 
@@ -15,16 +16,17 @@ function Search() {
   const [showResult, setShowResult] = useState(true);
   const [searchValue, setSearchValue] = useState('');
   const [loading, setLoading] = useState(false);
+  const debounced = useDebounce(searchValue, 500);
 
   const inputRef = useRef();
 
   useEffect(() => {
-    if (!searchValue.trim()) {
+    if (!debounced.trim()) {
       setSearchResult([]);
       return;
     }
     setLoading(true);
-    fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+    fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
       .then((res) => res.json())
       .then((res) => {
         setSearchResult(res.data);
@@ -33,7 +35,7 @@ function Search() {
       .catch((err) => {
         setLoading(false);
       });
-  }, [searchValue]);
+  }, [debounced]);
 
   const handleClear = () => {
     setSearchValue('');
